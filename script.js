73017de32372f50699ce2675b7969a2821b5752c
Roman <bolist@mail.ru>
2018-08-30 17:20:59 +0300

    second commit

--- script.js
+++ script.js
@@ -1,38 +1,14 @@
-//js 12 площа і периметр трикутника
-function perimeterTriangle(){
-    var st1 = parseInt(document.forma1.t1.value);
-    var st2 = parseInt(document.forma1.t2.value);
-    var st3 = parseInt(document.forma1.t3.value);
-    var perimetr = st1+st2+st3;
-    alert(perimetr);
-    document.getElementById('perimetr').value;
-function areaTriangle(){
-    var a = parseInt(document.forma1.t1.value);
-    var b = parseInt(document.forma1.t2.value);
-    var c = parseInt(document.forma1.t3.value);
-    var d=a+b+c;
-    var s=d/2;
-    alert(s);
-    document.forma1.res.value=s;
-}
-/*
-var x = prompt("Введите значение x ", "0");
-var y = prompt("Введите значение y ", "0.0");
-var integerX = parseInt("x");
-var floatY = parseFloat("y");
-var result = integerX + floatY;
-alert (result);
-document.write("result");
-*/
-var firstName = prompt("Введите имя", "сюда");
-var lastName = prompt("Введите фамилию","сюда");
-var age = prompt("Введите возраст","сюда");
-document.write ("Привет, firstName");
-function zapoln()
-{
-    x=prompt("Введите первое число");
-    y=prompt("Введите второе число");
-    var proiz=0;
-    proiz=x*y;
-    alert(proiz);
-}
+var hous = new Object();
+width = document.getElementById("width").value;
+length = document.getElementById("length").value;
+high = document.getElementById("high").value;
+doorsNumber = document.getElementById("doorsNumber").value;
+doorsHigh = document.getElementById("doorsHigh").value;
+doorsWidth = document.getElementById("doorsWidth").value;
+windowsNumber = document.getElementById("windowsNumber").value;
+windowsHigh = document.getElementById("windowsHigh").value;
+windowsWidth = document.getElementById("windowsWidth").value;
+wallThickness = document.getElementById("list").value;
+cost = document.getElementById("cost").value;
+var area = ((width + length) * 2 * high) - (doorsNumber * doorsHigh * doorsWidth) - (windowsNumber * windowsHigh * windowsWidth);
+alert(area);
