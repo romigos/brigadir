ea806ede1d98e3041d68ac68f0d04897dd94249e
Roman <bolist@mail.ru>
2018-09-05 17:16:27 +0300

    second commit

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 function perimetr() {
-    const width = document.getElementById("width").value;
-    const length = document.getElementById("length").value;
-    const high = document.getElementById("high").value;
-    var doorsNumber = document.getElementById("doorsNumber").value;
-    var doorsHigh = document.getElementById("doorsHigh").value;
-    var doorsWidth = document.getElementById("doorsWidth").value;
-    var windowsNumber = document.getElementById("windowsNumber").value;
-    var windowsHigh = document.getElementById("windowsHigh").value;
-    var windowsWidth = document.getElementById("windowsWidth").value;
-    var wallThickness = document.getElementById("list").value;
-    var cost = document.getElementById("cost").value;
+    const width = parseInt(document.getElementById("width").value);
+    const length = parseInt(document.getElementById("length").value);
+    //const high = document.getElementById("high").value;
+    //var doorsNumber = document.getElementById("doorsNumber").value;
+    //var doorsHigh = document.getElementById("doorsHigh").value;
+    //var doorsWidth = document.getElementById("doorsWidth").value;
+    //var windowsNumber = document.getElementById("windowsNumber").value;
+    //var windowsHigh = document.getElementById("windowsHigh").value;
+    //var windowsWidth = document.getElementById("windowsWidth").value;
+    //var wallThickness = document.getElementById("list").value;
+    //var cost = document.getElementById("cost").value;
 //var area = ((width + length) * 2 * high) - (doorsNumber * doorsHigh * doorsWidth) - (windowsNumber * windowsHigh * windowsWidth);
 //alert(area);
     const area = width * length;
+    document.getElementById(area);
     alert(area);
 }
